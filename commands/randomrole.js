6b38451a5d4167865c8425e011ce375b8c6d5f53
aekkasitsit5009-ui <aekkasit.sit5009@ndlp.go.th>
2026-07-26 18:10:58 +0000

    Update

---
+++ commands/randomrole.js
@@ -0,0 +1,54 @@
+const {
+    SlashCommandBuilder
+} = require("discord.js");
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+
+        .setName("randomrole")
+
+        .setDescription("สุ่มผู้เล่นจากบทบาท")
+
+        .addRoleOption(option =>
+            option
+                .setName("role")
+                .setDescription("เลือกบทบาท")
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+
+        const role =
+            interaction.options.getRole("role");
+
+        const members =
+            role.members
+                .filter(member => !member.user.bot)
+                .map(member => member);
+
+        if (members.length === 0) {
+
+            return interaction.reply({
+                content: "❌ บทบาทนี้ไม่มีผู้เล่น",
+                ephemeral: true
+            });
+
+        }
+
+        const random =
+            members[
+                Math.floor(Math.random() * members.length)
+            ];
+
+        await interaction.reply({
+
+            content:
+`🎲 Random ${role.name} จาก ${members.length} คน
+ผู้ได้รับเลือกคือ **${random}**`
+
+        });
+
+    }
+
+};
